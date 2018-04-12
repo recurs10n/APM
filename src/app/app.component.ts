3bf4977e78b4cab7e00cae2e627853caedc9d13b
Daniel Brown <djbrown@dstsystems.com>
2018-04-12 18:14:37 -0400

    post Retrieving Data Using HTTP

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  providers: [ProductService]
+})
+export class AppComponent {
+  pageTitle = 'Acme Product Mangement';
+}
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    errorMessage: string;
+
+    products: IProduct[];
+    filteredProducts: IProduct[];
+
+    _listFilter:string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter = value;
+
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    constructor( private _productService: ProductService )
+    {
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        this._productService.getProducts()
+            .subscribe( 
+                products => {
+                    this.products = products;
+                    this.filteredProducts = this.products;
+                },
+                error => this.errorMessage = <any>error
+            );
+    }
+
+    performFilter( filterBy: string ) :IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter( (product:IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1 );
+    }
+
+    onRatingClicked( message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
+}
