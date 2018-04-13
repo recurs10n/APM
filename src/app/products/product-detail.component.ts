39a52a100ff48c1e7752c356eac8a1e11d15e68e
Daniel Brown <djbrown@dstsystems.com>
2018-04-13 15:06:48 -0400

    Post Navigatio nand Routing Additional Techniques

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from './product';
 
 @Component({
@@ -9,9 +11,30 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail'
   product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+      private _router: Router ) 
+  { 
+    
+  }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+
+    this.product = {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2016",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    };
+  }
+
+  onBack(): void{
+    this._router.navigate( ['/products'] );
   }
 
 }
